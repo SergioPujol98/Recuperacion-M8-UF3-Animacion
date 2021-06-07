e9520f2aae25f99c69ecc6ed76a0799c9235f10d
Black Shadow <sergiopujol.98@gmail.com>
2021-06-07 15:33:02 +0200

    First commit

---
+++ desktop/src/com/pujol/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pujol.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pujol.game.RecuGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RecuGame(), config);
+	}
+}
